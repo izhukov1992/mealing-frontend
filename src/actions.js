494a4688f7446245d481b5a532b10a69022743f4
Ilya Zhukov <zhukov1@avaya.com>
2018-03-19 20:05:25 +0300

    Continue implementing a signing in view

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,22 @@
-export const signIn = (login, pass) => ({
-  type: 'SIGN_IN',
-  login,
-  pass
-})
+import * as constants from './constants';
+
+export function signIn(login, pass) {
+  return {
+    type: constants.SIGN_IN,
+    login,
+    pass
+  }
+}
+
+export function signedIn(token) {
+  return {
+    type: constants.SIGNED_IN,
+    token
+  }
+} 
+
+export function signOut() {
+  return {
+    type: constants.SIGN_OUT
+  }
+} 
--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,19 @@
-export const signIn = (login, pass) => ({
-  type: 'SIGN_IN',
-  login,
-  pass
-})
+import { SIGN_IN, SIGNED_IN } from './constants'
+
+export function signIn(state = {}, action) {
+  switch (action.type) {
+    case SIGN_IN:
+      return {};
+    default:
+      return state
+  }
+}
+
+export function signedIn(state = {}, action) {
+  switch (action.type) {
+    case SIGNED_IN:
+      return {};
+    default:
+      return state
+  }
+}
